946fa5014f0caa5451edd08d2010653e02154422
Gilad David <gilad.david95@gmail.com>
2022-09-23 14:17:16 +0300

    added main switch by argv

--- wiki_scraper/main.py
+++ wiki_scraper/main.py
@@ -21,28 +21,51 @@ SLOT_TO_SLOT_NAME = {
 
 import json
 
+DIR_NAME = 'output/'
+
 def d(data, name):
     with open(f'{name}.json', 'w') as f:
         json.dump(data, f)
 
 
-def main():
-    dir_name = 'output/'
-    os.mkdir(dir_name)
+
+def items_main():
+    if not os.path.isdir(DIR_NAME):
+        os.mkdir(DIR_NAME)
 
     for slot in SLOTS:
         
         Wiki.open_wiki(slot=slot, category='items')
         items = Wiki.scrape_items()
         
-        slot_dir_name = f'{dir_name}/{slot}'
-        os.mkdir(slot_dir_name)
+        slot_dir_name = f'{DIR_NAME}/{slot}'
+        if not os.path.isdir(slot_dir_name):
+            os.mkdir(slot_dir_name)
         d(items, f'{slot_dir_name}/bases')
 
         for affix in AFFIXES:
             Wiki.open_wiki(slot=slot, category=affix)       
             affix_mods = Wiki.scrape_affixes(SLOT_TO_SLOT_NAME[slot])
             d(affix_mods, f'{slot_dir_name}/{affix}')
+
+def blessings_main():
+    if not os.path.isdir(DIR_NAME):
+        os.mkdir(DIR_NAME)
+
+    slot = 'blessings'        
+    Wiki.open_wiki(slot=slot, category='items')
+    items = Wiki.scrape_blessings()
+        
+    slot_dir_name = f'{DIR_NAME}/{slot}'
+    if not os.path.isdir(slot_dir_name):
+        os.mkdir(slot_dir_name)
+    d(items, f'{slot_dir_name}/bases')
+
+
+mains = {
+    'blessings': blessings_main,
+    'items': items_main
+}
 
 if __name__ == '__main__':
     if sys.platform == 'NT':
@@ -53,4 +76,4 @@ if __name__ == '__main__':
         executable_path = './driver/geckodriver'
     
     with driver(browser, executable_path):
-        main()
+        mains[sys.argv[1]]()
