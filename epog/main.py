4d7bbf0fec0cd2c0b7b49618df345c8d702373fc
Gilad David <gilad.david95@gmail.com>
2022-09-23 21:33:20 +0300

    Clean main imports

--- epog/main.py
+++ epog/main.py
@@ -1,11 +1,5 @@
 import pygame
 from app import Epog
-from framework.button.text_button import TextButton
-from framework.screen.passive_tree_screen import PassiveTreeScreen
-
-from framework.util.util import ORIGIN
-from framework.screen import Screen
-from framework.logic.screen_buttons import screen_buttons
 
 def main():
     pygame.init()
